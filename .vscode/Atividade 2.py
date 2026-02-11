77263fc0a7ef2c38193f2b5ed55038e2f84981ce
Leandra Isabela dos Santos <leandra_i_santos@estudante.sesisenai.org.br>
2026-02-11 14:50:34 +0000

    update

---
+++ .vscode/Atividade 2.py
@@ -0,0 +1,16 @@
+#Atividade somando valores flutuantes
+numero1 = float (input("Insira o primeiro número: "))
+numero2 = float (input("Insira o primeiro número: "))
+soma = numero1+numero2
+print ("A soma dos dois numeros é: " ,soma)
+
+#Atividade média de notas de alunos
+nota1 = float (input("Insira a primeira nota: "))
+nota2 = float (input("Insira a segunda nota: "))
+nota3 = float (input("Insira a terceira nota: "))
+nota4 = float (input("Insira a quarta nota: "))
+media = (nota1+nota2+nota3+nota4)/4
+print = ("A média é: ", media)
+
+
+
---
+++ Atividades Python/Atividade 1.py
@@ -0,0 +1,6 @@
+#Primeira atividade de python usando a concatenação de valores
+print("Olá Mundo") 
+numero1 = input("Digite o número1: ")
+numero2 = input("Digite o número2: ")
+soma = numero1+numero2
+print("a soma do número 1 e número 2 é:  " ,soma)
